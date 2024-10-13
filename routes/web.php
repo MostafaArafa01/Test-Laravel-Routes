c2ddc985f6e481b2d84589847bc3b26f8f929316
MostafaArafa01 <mostafaarafa478@gmail.com>
2024-10-13 17:16:19 +0300

    Solve bugs

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,8 @@ Route::redirect('/log-in','/login');
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
-Route::group(['middleware => 'auth']), function(){
-	Route::group(['prefix => 'app']), function(){
+Route::group(['middleware' => 'auth']), function(){
+	Route::group(['prefix' => 'app']), function(){
 		Route::get('/dashboard',App\Http\Controllers\DashboardController::class)->name('dashboard');
 		Route::Resource('/tasks', App\Http\Controllers\TaskController::class);
 	});
